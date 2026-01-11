e60a0fb07d367b30b60ee46663591575b2fa7171
markobasko <55markovasic@gmail.com>
2026-01-11 17:19:29 +0100

    created Address class

---
+++ BookFair/Model/Address.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Address
+{
+	public int Id { get; set; }
+	public string Street { get; set; }
+	public int stNumber { get; set; }
+	public string City { get; set; }
+	public string Country { get; set; }
+
+	public Address()
+	{
+		Id = -1;
+		Street = string.Empty;
+        stNumber = 0;
+        City = string.Empty;
+		Country = string.Empty;
+
+	}
+}
