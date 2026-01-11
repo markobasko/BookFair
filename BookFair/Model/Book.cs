24914fb9dde87749bee74e39b347bc2a46dd9a9b
zoranaes15 <zoranalicina04@gmail.com>
2026-01-11 18:22:03 +0200

    added book class

---
+++ BookFair/Model/Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+public enum Genre
+{
+    Drama,
+    Comedy,
+    Horror,
+    Thriller,
+    Romance,
+    SciFi,
+    Fantasy,
+    NonFiction,
+    Mystery,
+    Biography
+}
+
+public class Book
+{
+    public int Id { get; set; } 
+    public string ISBN { get; set; }
+    public string Name { get; set; }
+    public Genre Genre { get; set; }
+    public int Price { get; set; }
+    public int NumberOfPages { get; set; }
+
+
+	public Book()
+	{
+        Id = -1;
+        ISBN = string.Empty;
+        Name = string.Empty;
+        Genre = Genre.Drama;
+        Price = 0;
+        NumberOfPages = 0;
+
+	}
+}
