b3ea88d587f083eb702d4744d2ea380d27b3f48b
markobasko <55markovasic@gmail.com>
2026-01-11 17:54:18 +0100

    added Visitor xd

---
+++ BookFair/Model/Visitor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Visitor
+{
+
+	public int Id { get; set; }
+	public int MembershipNumber{ get; set; }
+	public List<Purchase> Purchase { get; set; }
+
+	public Visitor()
+	{
+		Id = -1;
+		MembershipNumber = 0;
+		Purchase = new List<Purchase>();
+	}
+}
