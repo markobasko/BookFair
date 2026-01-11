bf5d4d95aa58174743e2ad5e993b76644a19dfeb
zoranaes15 <zoranalicina04@gmail.com>
2026-01-11 18:52:24 +0200

    added author and person UwU

---
+++ BookFair/Model/Author.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Author
+{
+	public int Id { get; set; }
+	public string IdCardNumber { get; set; }
+	public List<Book> WrittenBooks { get; set; }
+	public int YearsOfExperience { get; set; }
+
+	public Author()
+	{
+		Id = 0;
+		IdCardNumber = string.Empty;
+		YearsOfExperience = 0;
+		WrittenBooks = new List<Book>();
+	}
+}
---
+++ BookFair/Model/Person.cs
@@ -0,0 +1,22 @@
+﻿using system;
+public abstract class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public Address Address { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+
+    public Person(string firstName, string lastName, DateTime dateOfBirth, Address address, string phoneNumber, string email)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        DateOfBirth = dateOfBirth;
+        Address = address;
+        PhoneNumber = phoneNumber;
+        Email = email;
+    }
+
+    public abstract string ToString();
+}
