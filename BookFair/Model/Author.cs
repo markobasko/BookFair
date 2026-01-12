4b693d35207e686f5b9ce2d934e0dd3f1ea10c9a
zoranaes15 <zoranalicina04@gmail.com>
2026-01-13 00:41:08 +0200

    fix xdxdxdxddx

--- BookFair/Model/Author.cs
+++ BookFair/Model/Author.cs
@@ -1,24 +1,27 @@
 ﻿using BookFair.Serialization;
 using System;
 
-
-public class Author : Person,ISerializable
+namespace BookFair.Model
 {
-	public int Id { get; set; }
-	public string IdCardNumber { get; set; }
-	public List<Book> WrittenBooks { get; set; }
-	public int YearsOfExperience { get; set; }
+	public class Author : Person, ISerializable
+	{
+		public int Id { get; set; }
+		public string IdCardNumber { get; set; }
+		public List<Book> WrittenBooks { get; set; }
+		public int YearsOfExperience { get; set; }
 
-	public Author()
-	{
-		Id = 0;
-		IdCardNumber = string.Empty;
-		YearsOfExperience = 0;
-		WrittenBooks = new List<Book>();
+		public Author()
+		{
+			Id = 0;
+			IdCardNumber = string.Empty;
+			YearsOfExperience = 0;
+			WrittenBooks = new List<Book>();
+		}
+		public override string ToString()
+		{
+			return $"Author[Id:{Id},FirstName:{FirstName},LastName:{LastName},BirthDate:{DateOfBirth},Address:{Address},PhoneNumber:{PhoneNumber},Email:{Email},YearsExperience:{YearsExperience},IDLK:{IdCardNumber}]";
+		}
+
+
 	}
-    public override string ToString()
-	{
-        return $"Author[Id:{Id},FirstName:{FirstName},LastName:{LastName},BirthDate:{BirthDate},Address:{Address},PhoneNumber:{PhoneNumber},Email:{Email},YearsExperience:{YearsExperience},IDLK:{IdCardNumber}]";
-    }
-    
 }
