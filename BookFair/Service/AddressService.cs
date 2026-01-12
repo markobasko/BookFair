c1b848267a7226513304a9f11211839fc1d81b64
markobasko <55markovasic@gmail.com>
2026-01-13 00:51:09 +0200

    service update

---
+++ BookFair/Service/AddressService.cs
@@ -0,0 +1,39 @@
+﻿using BookFair.DAO;
+using BookFair.Model;
+
+namespace BookFair.Service
+{
+    public class AddressService
+    {
+        private readonly AddressDAO _addressDAO;
+        public AddressService()
+        {
+            _addressDAO = new AddressDAO();
+        }
+        public List<Address> GetAll()
+        {
+            List<Address> addresses = _addressDAO.GetAll();
+            return addresses;
+        }
+        public Address GetById(int id)
+        {
+            return _addressDAO.GetById(id);
+        }
+        public void Add(Address addresses)
+        {
+            if (addresses == null)
+                throw new ArgumentNullException(nameof(addresses), "Address cannot be null.");
+            _addressDAO.Add(addresses);
+        }
+        public void Remove(int addressId)
+        {
+            _addressDAO.Remove(addressId);
+        }
+        public void Update(Address address)
+        {
+            if (address == null)
+                throw new ArgumentNullException(nameof(address), "Address cannot be null.");
+            _addressDAO.Update(address);
+        }
+    }
+}
---
+++ BookFair/Service/BookService.cs
@@ -0,0 +1,39 @@
+﻿using BookFair.DAO;
+using BookFair.Model;
+
+namespace BookFair.Service
+{
+    public class BookService
+    {
+        private readonly BookDAO _bookDAO;
+        public BookService()
+        {
+            _bookDAO = new BookDAO();
+        }
+        public List<Book> GetAll()
+        {
+            List<Book> books = _bookDAO.GetAll();
+            return books;
+        }
+        public Book GetById(int id)
+        {
+            return _bookDAO.GetById(id);
+        }
+        public void Add(Book book)
+        {
+            if (book == null)
+                throw new ArgumentNullException(nameof(book), "Book cannot be null.");
+            _bookDAO.Add(book);
+        }
+        public void Remove(int bookId)
+        {
+            _bookDAO.Remove(bookId);
+        }
+        public void Update(Book book)
+        {
+            if (book == null)
+                throw new ArgumentNullException(nameof(book), "Book cannot be null.");
+            _bookDAO.Update(book);
+        }
+    }
+}
