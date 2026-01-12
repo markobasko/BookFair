f4dc10536587aab8dcde1f7bdbab035e9390f32d
zoranaes15 <zoranalicina04@gmail.com>
2026-01-13 00:51:15 +0200

    servis za autora

---
+++ BookFair/Service/AuthorService.cs
@@ -0,0 +1,40 @@
+﻿using BookFair.DAO;
+using BookFair.Model;
+
+namespace BookFair.Service
+{
+    public class AuthorService
+    {
+        private readonly AuthorDAO _authorDAO;
+        public AuthorService()
+        {
+            _authorDAO = new AuthorDAO();
+        }
+        public List<Author> GetAll()
+        {
+            List<Author> authors = _authorDAO.GetAll();
+            return authors;
+        }
+        public Author GetById(int id)
+        {
+            return _authorDAO.GetById(id);
+        }
+        public void Add(Author author)
+        {
+            if (author == null)
+                throw new ArgumentNullException(nameof(author), "Author cannot be null.");
+            _authorDAO.Add(author);
+        }
+        public void Remove(int authorID)
+        {
+            _authorDAO.Remove(authorID);
+        }
+        public void Update(Author author)
+        {
+            if (author == null)
+                throw new ArgumentNullException(nameof(author), "Author cannot be null.");
+            _authorDAO.Update(author);
+        }
+    }
+}
+
