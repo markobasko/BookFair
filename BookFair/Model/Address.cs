2ec9ad7f97020d56fe61af08078724d8450bed74
markobasko <55markovasic@gmail.com>
2026-01-13 00:15:32 +0200

    updated serialization

--- BookFair/Model/Address.cs
+++ BookFair/Model/Address.cs
@@ -5,7 +5,7 @@ public class Address : ISerializable
 {
 	public int Id { get; set; }
 	public string Street { get; set; }
-	public int stNumber { get; set; }
+	public int StNumber { get; set; }
 	public string City { get; set; }
 	public string Country { get; set; }
 
@@ -13,13 +13,43 @@ public class Address : ISerializable
 	{
 		Id = -1;
 		Street = string.Empty;
-        stNumber = 0;
-        City = string.Empty;
+		StNumber = 0;
+		City = string.Empty;
 		Country = string.Empty;
 	}
 
-	public Address()
+	public Address(string street, int stNumber, string city, string country)
 	{
+		Street = street;
+		StNumber = stNumber;
+		City = city;
+		Country = country;
+	}
+
+	public string toString()
+	{
+		return $"Address[Id:{Id},Street:{Street},StNumber:{StNumber},City:{City},Country{Country}]";
 
 	}
+
+    public string[] ToCSV()
+    {
+        string[] csvValues =
+        {
+                Id.ToString(),
+                Street,
+                StNumber.ToString(),
+                City,
+                Country
+            };
+        return csvValues;
+    }
+    public void FromCSV(string[] values)
+    {
+        Id = int.Parse(values[0]);
+        Street = values[1];
+        StNumber = int.Parse(values[2]);
+        City = values[3];
+        Country = values[4];
+    }
 }
--- BookFair/Model/Visitor.cs
+++ BookFair/Model/Visitor.cs
@@ -1,22 +1,51 @@
 ﻿using System;
-using System.Runtime.Serialization;
+using System.Net.NetworkInformation;
+using BookFair.Serialization;
 
 public class Visitor : Person, ISerializable
 {
 
 	public int Id { get; set; }
 	public int MembershipNumber{ get; set; }
-	public List<Purchase> Purchase { get; set; }
+	public List<Book> PurchasedBooks { get; set; }
 
 	public Visitor()
 	{
 		Id = -1;
 		MembershipNumber = 0;
-		Purchase = new List<Purchase>();
+        PurchasedBooks = new List<Book>();
 	}
 
     public override string ToString()
     {
 		return $"Visitor[Id:{Id},FirstName{FirstName},LastName{LastName},DateOfBirth{DateOfBirth},Address{Address},PhoneNumber{PhoneNumber},Email{Email}]";
+    }
+
+	public string[] ToCSV()
+	{
+		string[] csvValues =
+		{
+			Id.ToString(),
+			FirstName,
+			LastName,
+			DateOfBirth.ToString("yyy-MM-dd"),
+			Address.Id.ToString(),
+			PhoneNumber,
+			Email
+		};
+		return csvValues;
+	}
+
+    public void FromCSV(string[] values)
+    {
+        Id = int.Parse(values[0]);
+        FirstName = values[1];
+        LastName = values[2];
+        DateOfBirth = DateTime.Parse(values[3]);
+        Address.Id = int.Parse(values[4]);
+        PhoneNumber = values[5];
+        Email = values[6];
+        MembershipNumber = int.Parse(values[7]);
+        
     }
 }
